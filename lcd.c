ac9b5ee58341128a770ea01a9ffb63e09e900bd7
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-11-24 15:06:58 +0200

    lcd functions update

--- lcd.c
+++ lcd.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <xc.h>
 #include "lcd.h"
 #include "system.h"
@@ -21,7 +22,7 @@
 
 
 // low-level function to send data or command
-void lcd_write_data(unsigned char data){
+void lcd_write_data(uint8_t data){
     E = 0;
     LCD_PORT_DIR = 0;
     LCD_O_DATA = data;
@@ -33,7 +34,7 @@ void lcd_write_data(unsigned char data){
 }
 
 // no comments
-void lcd_send_cmd(unsigned char cmd){
+void lcd_send_cmd(uint8_t cmd){
     RS = 0;
     RW = 0;
     lcd_write_data(cmd);
@@ -41,7 +42,7 @@ void lcd_send_cmd(unsigned char cmd){
 }
 
 // no comments
-void lcd_send_data(unsigned char data){
+void lcd_send_data(uint8_t data){
     RS = 1;
     RW = 0;
     lcd_write_data(data);
@@ -50,7 +51,7 @@ void lcd_send_data(unsigned char data){
 // sets display`s light, cursor visibility and cursor blinking
 void display_controll(char d, char c, char b){
     // assembling final command
-    unsigned char cmd = (8 | (d << 2)) | (8 | (c << 1)) | (8 | b);
+    uint8_t cmd = (8 | (d << 2)) | (8 | (c << 1)) | (8 | b);
     lcd_send_cmd(cmd);
     __delay_us(50);
     return;
@@ -59,21 +60,21 @@ void display_controll(char d, char c, char b){
 // initializes the LCD as datasheet says
 // works with 20x4 and 16x2
 void lcd_init(void){
-    
+
     RS = 0;
     RW = 0;
     E = 0;
-    
+
     __delay_ms(150);  // power-up
-    lcd_send_cmd(0b00110000);  // 
-    __delay_ms(6);  // first Function Set
-    
     lcd_send_cmd(0b00110000);  //
-    __delay_us(150);  // second 
+    __delay_ms(6);  // first Function Set
+
+    lcd_send_cmd(0b00110000);  //
+    __delay_us(150);  // second
     lcd_send_cmd(0b00110000);  //
     __delay_us(150);  // third Function Set
-    
-    
+
+
     lcd_send_cmd(0b00111000);  // setting screen params by default (Function Set)
     __delay_us(100);  //
     display_controll(0, 0, 0);  // setting display params for init
@@ -82,19 +83,19 @@ void lcd_init(void){
     __delay_ms(3);
     lcd_send_cmd(0b00000110);  // entry mode set command
     __delay_us(100);
-    
-    
+
+
     display_controll(1, 0, 0);
     __delay_us(50);
-    
+
     return;
 }
 
-// literally, sets the cursor`s position 
+// literally, sets the cursor`s position
 // with HD44780 command 0x80 + address + position index
-void lcd_set_cursor(unsigned char r, unsigned char c)
+void lcd_set_cursor(uint8_t r, uint8_t c)
 {
-    unsigned char pos;
+    uint8_t pos;
     switch(r){
         case 1:
             pos = 0x80;
@@ -114,27 +115,25 @@ void lcd_set_cursor(unsigned char r, unsigned char c)
     __delay_us(80);
 }
 
-// should be deprecated later
-void lcd_write_ascii(unsigned char *str, size_t data_size){
-    while(data_size){
+// sends specific number of characters to the display
+void lcd_write_ascii(uint8_t *str, size_t data_size){
+    while(data_size--){
         lcd_send_data(*str++);
-        data_size--;
     }
 }
 
-// does the same as lcd_write_ascii()
-void lcd_write_string(unsigned char *str){
-    while(*str != '\0'){
+// sends a string which ends with \0
+void lcd_write_string(uint8_t *str){
+    while((*str) != '\0'){
         lcd_send_data(*str++);
     }
 }
 
 // takes a position and clears N cells after it, including it
-void clear_n_cells(unsigned char row, unsigned char pos, unsigned char n){
+void clear_n_cells(uint8_t row, uint8_t pos, uint8_t n){
     lcd_set_cursor(row, pos);
-    while(n){
+    while(n--){
         lcd_send_data(' ');
-        n--;
     }
     return;
 }
